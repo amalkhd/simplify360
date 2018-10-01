27c7bac156b682882fea7adfb27ea663593ef52f
amalkriz <amalkhd@gmail.com>
2018-10-01 12:54:20 +0530

    form

---
+++ js/form.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#myTab > li > a").click(function (ev) {
+        $("#myTab > li >a").not($(this)).removeClass("active");
+        $(this).addClass("active");
+        let name = $(this).attr("href");
+        $("#myContent > div").not(name).removeClass("active");
+        $(name).addClass("active");
+    })
+});
